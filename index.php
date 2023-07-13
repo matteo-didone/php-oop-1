3e902b59b750b31d3b794c7d5bbe7a620eb28480
matteo-didone <matteo.dido9@gmail.com>
2023-07-13 16:11:12 +0200

    Creating two objects from Movie class

--- index.php
+++ index.php
@@ -22,14 +22,73 @@ organizzando il layout dividendo la struttura ed i contenuti in file e parziali 
 class Movie
 {
     // Declaring instance variables regarding movie
-    public String $movie_title;
-    public String $release_year;
-    public String $movie_genre;
-    public String $movie_director;
-    public String $movie_rating;
-    public Array $movie_cast;
-    public String $movie_description;
-    public String $movie_distributor;
-    public String $movie_production_studio;
+    public string $movie_title;
+    public int $release_year;
+    public string $movie_genre;
+    public string $movie_director;
+    public string $movie_rating;
+    public array $movie_cast;
+    public string $movie_description;
+    public string $movie_distributor;
+    public string $movie_production_studio;
+
+    // Defining constructor method
+    public function __construct(
+        string $title,
+        int $year,
+        string $genre,
+        string $director,
+        string $rating,
+        array $cast,
+        string $description,
+        string $distributor,
+        string $production_studio
+    ) {
+        $this->movie_title = $title;
+        $this->release_year = $year;
+        $this->movie_genre = $genre;
+        $this->movie_director = $director;
+        $this->movie_rating = $rating;
+        $this->movie_cast = $cast;
+        $this->movie_description = $description;
+        $this->movie_distributor = $distributor;
+        $this->movie_production_studio = $production_studio;
+    }
+
+    // Defining method to get movie title
+    public function getMovieTitle()
+    {
+        return $this->movie_title;
+    }
 }
+
+?>
+
+
+<?php
+// Creating instances of Movie class and assigning them to variables $classicMovie and $modernMovie
+$classicMovie = new Movie(
+    'The Godfather',
+    1972,
+    'Crime',
+    'Francis Ford Coppola',
+    'R',
+    ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall'],
+    'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    'Paramount Pictures',
+    'Paramount Pictures'
+);
+
+$modernMovie = new Movie(
+    'The Dark Knight',
+    2008,
+    'Action',
+    'Christopher Nolan',
+    'PG-13',
+    ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart', 'Michael Caine'],
+    'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    'Warner Bros. Pictures',
+    'Warner Bros. Pictures'
+);
+
 ?>
